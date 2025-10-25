ad8d503e8883549937402d5a72ddb19e7b4858b5
Shaihd-15 <shahidchand35@gmail.com>
2025-10-25 20:03:39 +0530

    heart added

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @GetMapping("/")   
     public String home() {
-        return "Welcome to Spring Boot UPdated!";
+        return "Welcome to Spring Boot this is new update ❤️";
     }
 
     @GetMapping("/hello")   

6ed41e739e9c3f953b2efc8b21c8c4053ce2fa58
Shaihd-15 <shahidchand35@gmail.com>
2025-10-25 17:43:13 +0530

    yes update is added

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @GetMapping("/")   
     public String home() {
-        return "Welcome to Spring Boot!";
+        return "Welcome to Spring Boot UPdated!";
     }
 
     @GetMapping("/hello")   
